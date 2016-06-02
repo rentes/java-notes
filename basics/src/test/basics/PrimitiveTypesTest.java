daf94f3d75b6ba9f12e452bce84e8bead90a1f7c
Miguel Rentes <miguel.rentes@gmail.com>
2016-06-02 17:17:56 +0100

    Adds first test on the primitive types test battery

--- basics/src/test/basics/PrimitiveTypesTest.java
+++ basics/src/test/basics/PrimitiveTypesTest.java
@@ -3,8 +3,15 @@
  */
 package basics;
 
-import org.junit.*;
+import org.junit.Assert.assertFalse;
+import org.junit.Test;
 
 public class PrimitiveTypesTest {
-
+    @Test
+    public void absoluteOfMostNegativeValue() {
+        final int mostNegative = Integer.MIN_VALUE;
+        final int negated = Math.abs(mostNegative);
+        assertFalse("No positive equivalent of Integer.MIN_VALUE", negated > 0);
+    }
 }
+
